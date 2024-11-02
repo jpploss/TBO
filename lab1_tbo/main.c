18678eae7dcb3605da1c10ce1e7b48f6a920b369
jpploss <jpploss@edu.ufes.br>
2024-11-02 10:57:01 -0300

    fazendo o utilBit.c

--- lab1_tbo/main.c
+++ lab1_tbo/main.c
@@ -5,9 +5,9 @@
 // https://docs.google.com/document/d/1sg-wGuBs1OEmNWeEjdfNAExDT-4OGqx_ZFIJIcRPbgs/edit?tab=t.0
 
 int main() {
-    int n = 10;
+    int n = 100;
 
-    vetAux* v = criaVet(10);
+    vetAux* v = criaVet(n);
 
     for(int i = 2; i > 0; i = proxNaoMarcado(v, i))
         marcaMultiplos(v, i);
--- lab1_tbo/utilBit.c
+++ lab1_tbo/utilBit.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "util.h"
+#include <math.h>
 
 struct _vetAux {
-    int vet;
+    int bitmask; // bit 0: 2, bit 1: 3, ..., bit n-2: n
+    int n
 };
 
 vetAux* criaVet(int n) { // n = final do intervalo
-    return calloc(1, sizeof(vetAux));
+    vetAux* v = calloc(1, sizeof(vetAux));
+    v->n = n;
+    return v;
 }
 
 void marcaMultiplos(vetAux* v, int n) {
-    
+    for(int i = 2; i <= v->n; i++) {
+        if(i % n == 0) {
+            v->bitmask = v->bitmask | ((int) pow(2, (i-2))); // marca o bit correspondente ao número 'i'
+        }
+    }
 }
 
 int proxNaoMarcado(vetAux* v, int n) {
@@ -19,7 +27,7 @@ int proxNaoMarcado(vetAux* v, int n) {
 }
 
 void imprimeNaoMarcados(vetAux* v) {
-
+    
 }
 
 void desalocaVet(vetAux* v) {
