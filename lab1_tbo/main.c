451c25c44102203a9e06f3d254df5bd5268e1e6d
jpploss <jpploss@edu.ufes.br>
2024-11-03 16:38:11 -0300

    feito

--- lab1_tbo/main.c
+++ lab1_tbo/main.c
@@ -5,12 +5,13 @@
 // https://docs.google.com/document/d/1sg-wGuBs1OEmNWeEjdfNAExDT-4OGqx_ZFIJIcRPbgs/edit?tab=t.0
 
 int main() {
-    int n = 100;
+    int n = 33;
 
     vetAux* v = criaVet(n);
 
-    for(int i = 2; i > 0; i = proxNaoMarcado(v, i))
+    for(int i = 2; i > 0; i = proxNaoMarcado(v, i)) {
         marcaMultiplos(v, i);
+    }
     
     imprimeNaoMarcados(v);
     desalocaVet(v);
--- lab1_tbo/utilBit.c
+++ lab1_tbo/utilBit.c
@@ -9,25 +9,50 @@ struct _vetAux {
 };
 
 vetAux* criaVet(int n) { // n = final do intervalo
+    if((n - 1) > (sizeof(int)*8)) {
+        printf("Limite excedido, o número máximo é %ld.\n", sizeof(int)*8 + 1);
+        exit(EXIT_FAILURE);
+    }
     vetAux* v = calloc(1, sizeof(vetAux));
     v->fim = n;
     return v;
 }
 
 void marcaMultiplos(vetAux* v, int n) {
-    for(int i = 2; i <= v->fim; i++) {
+    if(n > v->fim) return;
+    int bitDeN = n - 2;
+    int mask = (int) pow(2, bitDeN+1);
+    for(int i = n + 1; i <= v->fim; i++) {
         if(i % n == 0) {
-            v->bitmask = v->bitmask | ((int) pow(2, (i-2))); // marca o bit correspondente ao número 'i'
+            v->bitmask = v->bitmask | mask; // marca o bit correspondente ao número 'i'
         }
+        mask *= 2;
     }
 }
 
 int proxNaoMarcado(vetAux* v, int n) {
-    
+    if(n > v->fim) return -1;
+
+    int bitDeN = n - 2;
+    int mask = (int) pow(2, bitDeN+1);
+
+    for(int b = bitDeN+1; b <= v->fim-2; b++) {
+        if( !(v->bitmask & mask) )
+            return (b+2);
+        mask *= 2;
+    }
+    return -1;
 }
 
 void imprimeNaoMarcados(vetAux* v) {
-
+    int mask = 1;
+    for(int i = 2; i < v->fim; i++) {
+        if( !(mask & v->bitmask) ) {
+            printf("%d ", i);
+        }
+        mask *= 2;
+    }
+    printf("\n");
 }
 
 void desalocaVet(vetAux* v) {
