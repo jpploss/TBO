1a1ea836ef23f4087b3158ddc614bad86d6cb6f2
jpploss <jpploss@edu.ufes.br>
2024-11-04 09:11:22 -0300

    com a verificação de tempo

--- lab1_tbo/main.c
+++ lab1_tbo/main.c
@@ -1,19 +1,24 @@
 #include "util.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // https://docs.google.com/document/d/1sg-wGuBs1OEmNWeEjdfNAExDT-4OGqx_ZFIJIcRPbgs/edit?tab=t.0
 
 int main() {
-    int n = 33;
+    clock_t inicio = clock();
 
+    int n = 330;
     vetAux* v = criaVet(n);
-
     for(int i = 2; i > 0; i = proxNaoMarcado(v, i)) {
         marcaMultiplos(v, i);
     }
-    
     imprimeNaoMarcados(v);
     desalocaVet(v);
+
+    clock_t fim = clock();
+    double tempoTotal = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+    printf("Elapsed time: %f seconds\n", tempoTotal);
+
     return 0;
 }
