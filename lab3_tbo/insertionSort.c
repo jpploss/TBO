eccab2cd1b612077d58fa53b24536e25fe6ec6d8
jpploss <jpploss@edu.ufes.br>
2024-11-24 13:26:19 -0300

    fiz até o bobble sort

---
+++ lab3_tbo/insertionSort.c
@@ -0,0 +1,15 @@
+#include "item.h"
+#include <stdio.h>
+
+void sort(Item *a, int lo, int hi) {
+    if(hi - lo <= 0) return;
+
+    printf("\nRodando insertion sort.\n");
+
+    for(int i = lo+1; i < hi; i++) {
+        for(int j = i; j > lo; j--) {
+            if(!less(a[j], a[j-1])) break;
+            exch(a[j], a[j-1]);
+        }
+    }
+}
--- lab3_tbo/selectionSort.c
+++ lab3_tbo/selectionSort.c
@@ -1,13 +1,14 @@
 #include "item.h"
+#include <stdio.h>
 
 void sort(Item *a, int lo, int hi) {
     if(hi - lo <= 0) return;
 
+    printf("\nRodando selection sort.\n");
+
     for(int i = lo; i < hi-1; i++) {
         int idxMenor = i;
-        for(int j = i+1; j < hi; j++) {
-            if(less(a[j], a[idxMenor])) idxMenor = j;
-        }
+        for(int j = i+1; j < hi; j++) if(less(a[j], a[idxMenor])) idxMenor = j;
         exch(a[i], a[idxMenor]);
     }
 }
