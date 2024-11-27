efa91421b514d532acf240211bd2adf341bd437c
jpploss <jpploss@edu.ufes.br>
2024-11-27 20:14:35 -0300

    fazendo as tabelas

--- lab3_tbo/main.c
+++ lab3_tbo/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "item.h"
+
+// https://docs.google.com/document/d/1tJPlL2nrLqV7jWZIIWgV1UjbmRoW0ld5rBrMppAp8dY/edit?usp=sharing
 
 extern void sort(Item *a, int lo, int hi);
 
@@ -16,11 +19,17 @@ int main(int argc, char* argv[]) {
     Item* itens = malloc(sizeof(Item)*n);
     for(int i = 0; i < n; i++) scanf("%d", (itens + i));
 
+    clock_t inicio = clock();
+
     sort(itens, 0, n);
 
-    printf("\n");
-    for(int i = 0; i < n; i++) printf("%d ", itens[i]);
-    printf("\n\n");    
+    clock_t fim = clock();
+    double tempoTotal = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+    printf("Tempo decorrido para N = %d: %f seconds\n\n", n, tempoTotal);
+
+    // printf("\n");
+    // for(int i = 0; i < n; i++) printf("%d ", itens[i]);
+    // printf("\n\n");    
 
     free(itens);
     return 0;
